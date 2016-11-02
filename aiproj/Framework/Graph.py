18751807d84722043091015fa447da800eca0f70
hassaans <18100222@lums.edu.pk>
2016-11-02 12:49:28 +0500

    Update Graph.py
    Implemented NetworkX

--- aiproj/Framework/Graph.py
+++ aiproj/Framework/Graph.py
@@ -2,18 +2,26 @@
 # most likely a wrapper around spicy: http://docs.scipy.org/doc/scipy/reference/sparse.csgraph.html
 # DO NOT ACCESS DIRECTLY! Usage via IR wrappers; apply relevant safeguards
 
+import networkx as nx
+
 class Graph:
     # global stack depth to define the recursion limit
     def __init__(self, nodes, edges, stackdepth):
         stackdepth
-        self.nodes = nodes
-        self.edges = edges
+
+        self.G=nx.Graph()
+        self.G.add_nodes_from(nodes)
+        for edge in edges:
+        	self.G.add_edge(edge.first,edge.second,{‘weight’:edge.weight})
 
     def getNodeCount(self):
-        return len(self.nodes)
+        return len(self.G)
 
     def getEdgeCount(self):
-        return len(self.edges)
+        return self.G.number_of_edges()
 
     def testReachibility(self):
-        1
+        if len(descendants(self.G, self.G.nodes()[0]))+1 == getNodeCount:
+        	return True
+        else
+        	return False
